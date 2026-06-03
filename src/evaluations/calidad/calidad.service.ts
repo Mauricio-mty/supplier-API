7f5e304587f4487052c7a0fd9c0c4b58bc9042a9
Mauricio-mty <00000422@uca.edu.sv>
2026-06-03 11:26:48 -0600

    find by one

--- src/evaluations/calidad/calidad.service.ts
+++ src/evaluations/calidad/calidad.service.ts
@@ -54,27 +54,27 @@ export class CalidadService {
   // 🔍 CONSULTA INDIVIDUAL: Buscar los detalles de un solo ítem seleccionado
   async findOneByOrderItem(orderItemId: string) {
     const query = `
-      SELECT 
-          p.numero_orden_compra,
-          p.fecha_entrega_prevista,
-          pro.codigo AS codigo_producto,
-          pro.nombre AS nombre_producto,
-          su.nombre AS nombre_proveedor,
-          po.id AS order_item_id,
-          po.cantidad,
-          ev.estado_completitud AS bodega_completitud,
-          ev.ingreso_aprobado AS bodega_ingreso_aprobado,
-          cal.cumple_calidad,
-          cal.cumple_norma,
-          cal.accion_tomada,
-          cal.fecha_evaluacion,
-          cal.revisado AS calidad_revisado
-      FROM public.purchase_orders AS p
-      INNER JOIN public.purchase_order_items AS po ON p.id = po.purchase_order_id
-      INNER JOIN public.suppliers AS su ON p.supplier_id = su.id
-      INNER JOIN public.products AS pro ON po.product_id = pro.id
-      LEFT JOIN public.bodega_evaluations AS ev ON po.id = ev.purchase_order_item_id
-      LEFT JOIN public.calidad_evaluations AS cal ON po.id = cal.purchase_order_item_id
+     
+      select
+      ev.id as calidad_evaluation_id,
+      p.numero_orden_compra,
+      p.fecha_entrega_prevista,
+      pro.codigo as codigo_producto,
+      pro.nombre as nombre_producto,
+      po.id as order_item_id,
+      po.cantidad,
+      ev.fecha_evaluacion,
+      ev.cumple_calidad,
+      ev.cumple_norma,
+      ev.accion_tomada,
+      ev.revisado,
+      ev.evaluado_por,
+      ev.comentarios
+      from public.purchase_orders as p
+      Inner join public.purchase_order_items as po ON p.id=po.purchase_order_id
+      Inner join  public.suppliers as su on p.supplier_id=su.id
+      inner join public.products as pro on po.product_id=pro.id
+      RIGHT JOIN public.calidad_evaluations as ev on po.id=ev.purchase_order_item_id
       WHERE po.id = $1;
     `;
     const resultado = await this.dataSource.query(query, [orderItemId]);
